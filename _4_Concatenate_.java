7446a5f621ee5391483360ed348bc992c4ea3d10
Naavarasi N R <84852191+nivimani171016@users.noreply.github.com>
2021-05-30 18:20:48 +0530

    Create _4_Concatenate_.java

---
+++ _4_Concatenate_.java
@@ -0,0 +1,17 @@
+
+public class _4_Concatenate_ {
+
+	public static void main(String[] args) {
+		String dna1="ATGCTGATGCA";
+		String dna2="ATGCGATGACTG";
+		
+		//Use of the '+' to concatenate 
+		System.out.println(dna1+dna2);
+		
+		//Use of the concat() to concatenate 
+		System.out.println(dna1.concat(dna2));
+		
+
+	}
+
+}
